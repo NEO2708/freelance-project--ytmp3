c7bb7959fcc75909cae9fe6272c949ac338a87ce
NEO2708 <parthr.2608@gmail.com>
2023-06-26 14:51:26 +0530

    change

--- ytmp3/settings.py
+++ ytmp3/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -108,7 +109,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR,'db.sqlite3'),
+    }
+}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
