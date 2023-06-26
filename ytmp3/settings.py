7940cee9192f77b41d708131bf19f90799052c22
NEO2708 <parthr.2608@gmail.com>
2023-06-26 16:39:35 +0530

    try8

--- ytmp3/settings.py
+++ ytmp3/settings.py
@@ -109,12 +109,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR,'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR,'db.sqlite3'),
+#     }
+# }
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
