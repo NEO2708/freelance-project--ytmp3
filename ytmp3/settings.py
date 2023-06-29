0d31d608adfc454f9a0cd83d75cae6c07605eecf
NEO2708 <parthr.2608@gmail.com>
2023-06-29 19:50:05 +0530

    final-02

--- ytmp3/settings.py
+++ ytmp3/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!ty+0hg$s3$lr%8)ms^jct02-3_r6k(8=tau2d@$gaacbsjtn@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1','localhost','b6d1-2401-4900-1c80-c727-40da-4189-7d12-a8e9.ngrok-free.app']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
