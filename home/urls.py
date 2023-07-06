b010e7e8d0953d21f31f4b1914023c014901dda7
NEO2708 <parthr.2608@gmail.com>
2023-07-06 16:12:50 +0530

    update0.3

--- home/urls.py
+++ home/urls.py
@@ -1,18 +1,11 @@
-from django.contrib import admin
+
 from django.urls import path
-from home import views
 from home import igdownloader
-from home import postDownloader
-from home import ytdownloader
 from home import home
-from home import igtest
+
 
 urlpatterns = [
-    path('', views.download_video),
     path('insta',igdownloader.InstaDownloader ),
-    path('downloadfile',postDownloader.download_file),
-    path('ytdownload',ytdownloader.ytdownload),
     path('homepage',home.homepage),
-    path('test',igtest.InstaDownloader)
 
 ]
